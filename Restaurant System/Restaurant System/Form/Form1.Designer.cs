53a37c3ca5d4dbfa7e15c624503e92e5655d856a
Skyfiprog2020 <skyfiprogrammiing2020@gmail.com>
2020-04-07 17:13:55 +1200

    Updated System

---
+++ Restaurant System/Restaurant System/Form/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Restaurant_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SubPanel = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.ToolPanel = new System.Windows.Forms.Panel();
+            this.icMinimize = new System.Windows.Forms.Button();
+            this.icMaximize = new System.Windows.Forms.Button();
+            this.icNormal = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.SubPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.ToolPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SubPanel
+            // 
+            this.SubPanel.BackColor = System.Drawing.Color.MediumBlue;
+            this.SubPanel.Controls.Add(this.button5);
+            this.SubPanel.Controls.Add(this.button4);
+            this.SubPanel.Controls.Add(this.button3);
+            this.SubPanel.Controls.Add(this.button2);
+            this.SubPanel.Controls.Add(this.button1);
+            this.SubPanel.Controls.Add(this.panel1);
+            this.SubPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SubPanel.Location = new System.Drawing.Point(0, 0);
+            this.SubPanel.Name = "SubPanel";
+            this.SubPanel.Size = new System.Drawing.Size(230, 479);
+            this.SubPanel.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Image = global::Restaurant_System.Properties.Resources.reports;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button5.Location = new System.Drawing.Point(3, 319);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(224, 52);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Reports";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Image = global::Restaurant_System.Properties.Resources.security;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button4.Location = new System.Drawing.Point(3, 261);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 52);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Security";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Image = global::Restaurant_System.Properties.Resources.trasaction;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button3.Location = new System.Drawing.Point(3, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(224, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Transaction";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = global::Restaurant_System.Properties.Resources.entry;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.Location = new System.Drawing.Point(3, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Entry";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = global::Restaurant_System.Properties.Resources.file1;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(3, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "File";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 71);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(51, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Company Name";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::Restaurant_System.Properties.Resources.company_logo;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(5, 16);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // ToolPanel
+            // 
+            this.ToolPanel.BackColor = System.Drawing.Color.MediumBlue;
+            this.ToolPanel.Controls.Add(this.icMinimize);
+            this.ToolPanel.Controls.Add(this.icMaximize);
+            this.ToolPanel.Controls.Add(this.icNormal);
+            this.ToolPanel.Controls.Add(this.button6);
+            this.ToolPanel.Controls.Add(this.pictureBox1);
+            this.ToolPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ToolPanel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.ToolPanel.Location = new System.Drawing.Point(230, 0);
+            this.ToolPanel.Name = "ToolPanel";
+            this.ToolPanel.Size = new System.Drawing.Size(704, 40);
+            this.ToolPanel.TabIndex = 2;
+            this.ToolPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ToolPanel_MouseDown);
+            this.ToolPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ToolPanel_MouseMove);
+            this.ToolPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ToolPanel_MouseUp);
+            // 
+            // icMinimize
+            // 
+            this.icMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.icMinimize.BackColor = System.Drawing.Color.Transparent;
+            this.icMinimize.BackgroundImage = global::Restaurant_System.Properties.Resources.minimize_icon;
+            this.icMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.icMinimize.FlatAppearance.BorderSize = 0;
+            this.icMinimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.icMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.icMinimize.Location = new System.Drawing.Point(642, 10);
+            this.icMinimize.Name = "icMinimize";
+            this.icMinimize.Size = new System.Drawing.Size(19, 12);
+            this.icMinimize.TabIndex = 6;
+            this.icMinimize.TabStop = false;
+            this.icMinimize.Tag = "";
+            this.icMinimize.UseVisualStyleBackColor = false;
+            this.icMinimize.Click += new System.EventHandler(this.icMinimize_Click);
+            // 
+            // icMaximize
+            // 
+            this.icMaximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.icMaximize.BackColor = System.Drawing.Color.Transparent;
+            this.icMaximize.BackgroundImage = global::Restaurant_System.Properties.Resources.maximize_icon1;
+            this.icMaximize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.icMaximize.FlatAppearance.BorderSize = 0;
+            this.icMaximize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.icMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.icMaximize.Location = new System.Drawing.Point(663, 9);
+            this.icMaximize.Name = "icMaximize";
+            this.icMaximize.Size = new System.Drawing.Size(19, 12);
+            this.icMaximize.TabIndex = 5;
+            this.icMaximize.TabStop = false;
+            this.icMaximize.Tag = "";
+            this.icMaximize.UseVisualStyleBackColor = false;
+            this.icMaximize.Click += new System.EventHandler(this.icMaximize_Click);
+            // 
+            // icNormal
+            // 
+            this.icNormal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.icNormal.BackColor = System.Drawing.Color.Transparent;
+            this.icNormal.BackgroundImage = global::Restaurant_System.Properties.Resources.normal_icon1;
+            this.icNormal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.icNormal.FlatAppearance.BorderSize = 0;
+            this.icNormal.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.icNormal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.icNormal.Location = new System.Drawing.Point(662, 10);
+            this.icNormal.Name = "icNormal";
+            this.icNormal.Size = new System.Drawing.Size(19, 12);
+            this.icNormal.TabIndex = 4;
+            this.icNormal.TabStop = false;
+            this.icNormal.Tag = "";
+            this.icNormal.UseVisualStyleBackColor = false;
+            this.icNormal.Visible = false;
+            this.icNormal.Click += new System.EventHandler(this.icNormal_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.BackgroundImage = global::Restaurant_System.Properties.Resources.close_icon2;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(682, 9);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(19, 12);
+            this.button6.TabIndex = 0;
+            this.button6.TabStop = false;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::Restaurant_System.Properties.Resources.hide_icon1;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 26);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(230, 40);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(704, 439);
+            this.MainPanel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 479);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.ToolPanel);
+            this.Controls.Add(this.SubPanel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File";
+            this.SubPanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ToolPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel SubPanel;
+        private System.Windows.Forms.Panel ToolPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button icNormal;
+        private System.Windows.Forms.Button icMaximize;
+        private System.Windows.Forms.Button icMinimize;
+    }
+}
+
---
+++ Restaurant System/Restaurant System/Form/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Restaurant_System
+{
+    public partial class Form1 : Form
+    {
+        bool drag = false;
+        Point start_point = new Point(0, 0);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (SubPanel.Width == 230)
+            {
+                SubPanel.Width = 50;
+            }
+            else
+            {
+                SubPanel.Width = 230;
+            }
+        }
+
+        private void icMaximize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            icMaximize.Visible = false;
+            icNormal.Visible = true;
+        }
+
+        private void icNormal_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            icNormal.Visible = false;
+            icMaximize.Visible = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void icMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ToolPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                Point p = PointToScreen(e.Location);
+                this.Location = new Point(p.X - start_point.X, p.Y - start_point.Y);
+            }
+        }
+
+        private void ToolPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+        }
+
+        private void ToolPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            drag = true;
+            start_point = new Point(e.X, e.Y);
+        }
+    }
+}
